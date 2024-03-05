63a15ef1de23b10366991999b17010111ed4f31b
eduardo24cba <eduardo.24.cba@gmail.com>
2024-03-05 14:16:12 -0300

    Update navbarAside.js
    cambiamos atributos a camelCase

--- components/navbarAside.js
+++ components/navbarAside.js
@@ -58,7 +58,7 @@ const NavbarAside = (camaras) => {
                     data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav"
                     aria-expanded="false" aria-label="Toggle-navigation" id="filtros">
                 Filtrar
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#f2f2f2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#f2f2f2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>
             </button>
             <div className="collapse navbar-collapse order-last" id="nav">
                 <NavbarItemsAside items={[filtro_diseno, filtro_resolucion, filtro_tipo_camara]} />
