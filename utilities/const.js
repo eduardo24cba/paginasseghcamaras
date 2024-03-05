2ed54415ac7251b530c75d1e086197811177b6e2
eduardo24cba <eduardo.24.cba@gmail.com>
2024-03-05 13:57:33 -0300

    Update const.js
    agregamos constantes que utilizaremos con react router

--- utilities/const.js
+++ utilities/const.js
@@ -1,2 +1,7 @@
 const ITEMS_NAV_TOP = ["Productos", "Nosotros", "Contacto"]
 const FIELDS_CARD = ["diseño", "resolucion", "conectividad", "marca"]
+
+const Routes = ReactRouterDOM.Routes
+const Route = ReactRouterDOM.Route
+const BrowserRouter = ReactRouterDOM.BrowserRouter
+const Link = ReactRouterDOM.Link
