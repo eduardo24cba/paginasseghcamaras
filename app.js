6c406b6c47851b5767d635f4c037a46293261ade
eduardo <eduardo.24.cba@gmail.com>
2024-03-06 02:54:47 -0300

    probando router

--- app.js
+++ app.js
@@ -15,12 +15,41 @@ const OnLoad = () => {
     )
 }
 
+const demora = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout( function(){
+            return resolve(f())
+        }, 5000
+        )
+    }
+
+    )
+}
+
+const Hola = () => <h3>Hola</h3>
+
+const Adios = () => <h3>Chau</h3>
+
+const Home = () => <h2>Home</h2>
+
+const App = () => {
+    console.log(BrowserRouter)
+    return(
+        <>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/h" element={<Hola />} />
+            <Route path="*" element={<h3>erroorroroororor</h3>} />
+        </Routes>
+        </>
+    )
+}
 const ListItem = () => {
     const [data, setData] = useState(null)
     
     useEffect(() => {
         async function filterData(){
-            let camaras = await f()
+            let camaras = await demora()
             setData(camaras)
         }
         filterData()
@@ -42,4 +71,8 @@ return(
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<ListItem/>)
+root.render(
+    <BrowserRouter basename="/">
+        <App />
+    </BrowserRouter>
+)
