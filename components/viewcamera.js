310d02776991bd978c7c252c46ba2feb76bc78d9
eduardo24cba <eduardo.24.cba@gmail.com>
2024-03-07 16:51:10 -0300

    Update viewcamera.js
    cambiamos estilo de los botones

--- components/viewcamera.js
+++ components/viewcamera.js
@@ -1,42 +1,48 @@
-'use strict';
+const ViewCamera = () => {
+    const location = useLocation()
+    const navigate = useNavigate()
 
-export function ViewCamera({camara}){
-    let obj = camara[0]
-    console.log(camara[0].modelo)
-      return (
-          <div className="container">
-              <div className="row mt-5">
-                  <div className="col-lg-8">
-                      <span className="d-md-block d-md-none position-absolute m-3" style={{"fontWeight": "bold"}}>1 / 2</span>
-                    <div className="d-flex container justify-content-center">
-                        <img src={obj.imagenes[0]} alt="" />
-                    </div>
-                    <div className="mx-auto d-none d-md-block mt-4 border-0">
-                        <img src={obj.imagenes[0]} style={{"width":"100px", "height": "100px"}} className="img-thumbnail" alt=""/>
-                    </div>
-                  </div>
-                  
-                  <div className="col-lg-4">
-                    <div className="d-flex-column">
-                      <p>Modelo: {obj.modelo}</p>
-                      <p>Diseño: {obj.diseño}</p>
-                      <p>Resolucion: {obj.resolucion}</p>
-                      <p>Conectividad: {obj.conectividad}</p>
-                      <p>Dimensiones: {obj.dimensiones}</p>
-                      <p>Descripcion: <br/><br/> {obj.descripcion}</p>
-                      <div className="d-flex justify-content-between">
-                        <button className="btn btn-primary">
-                          <i className="fa fa-share-alt"></i>
-                          Compartir  
-                        </button>
-                        <button className="btn btn-secondary">
-                          Volver al listado
-                        </button>
+    const camara = location.state.camara
+
+    
+    return (
+        <div className="container">
+            <div className="row mt-5">
+                <div className="col-lg-8">
+                    <span className="d-md-block d-md-none position-absolute m-3" style={{"fontWeight": "bold"}}>1 / 2</span>
+                      <div className="d-flex container justify-content-center">
+                          <img src={camara.imagenes[0]} alt="" />
                       </div>
-                      
+                      <div className="mx-auto d-none d-md-block mt-4 border-0">
+                          <img src={camara.imagenes[0]} style={{"width":"100px", "height": "100px"}} className="img-thumbnail" alt=""/>
+                      </div>
+                </div>
+                  
+                <div className="col-lg-4">
+                    <div className="d-flex-column">
+                        <p className="text-modelo">Modelo: {camara.modelo}</p>
+                        <p className="text-modelo">Diseño: {camara.diseño}</p>
+                        <p className="text-modelo">Resolucion: {camara.resolucion}</p>
+                        <p className="text-modelo">Conectividad: {camara.conectividad}</p>
+                        <p className="text-modelo">Dimensiones: {camara.dimensiones}</p>
+                        <p className="text-modelo">Descripcion: <br/><br/> {camara.descripcion}</p>
+                        <div className="row">
+                            <div className="col d-flex justify-content-between" id="share-product">
+                                <div className="share-product-social">
+                                    <button type="button" className="btn" id="link_shared">
+                                        <i className="fa fa-share-alt"></i>
+                                        Compartir  
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    <button type="button" onClick={() => navigate(-1)} className="btn btn-volver">
+                        Volver al listado
+                    </button>
                     </div>  
-                  </div>
-              </div>
-          </div>
+                </div>
+            </div>
+        </div>
       )
   }
