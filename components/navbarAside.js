3f16091547772a9813d6b4d242c7ac8ac9996df7
eduardo24cba <eduardo.24.cba@gmail.com>
2024-03-13 14:13:46 -0300

    Update navbarAside.js
    estamos quitando el filtro cuando la camara no lo necesite

--- components/navbarAside.js
+++ components/navbarAside.js
@@ -1,22 +1,28 @@
-const SectionNavAside = ({dictionary, idSection}) => {
-    let title = idSection.split("_")[1]
+const SectionNavAside = ({dictionary, idSection, camaras}) => {
+    let title = idSection.replace("section_", "")
     
     title === "resolucion"
      ? title = "Resolución" 
      : title = title[0].toUpperCase() + title.slice(1) //capitalize
 
     
-    
     const itemsSections = Object.keys(dictionary).map(key => {
+        
         return Object.keys(dictionary[key]).map(itemDictKey => {
+            
             return(
+                Object.values(dictionary[key]).length > 1 ?
                 <li key={itemDictKey} className="nav-item">
                         <div className="d-flex flex-row align-items-justify justify-content-center">
-                            <a href="#" className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                            <Link to={`/productos/camaras/filtro/${title.toLowerCase()}`} state={{camaras:camaras,
+                             key: title,
+                             value:itemDictKey}} 
+                             className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
                                 {itemDictKey}
-                            </a>
+                            </Link>
                         </div>
                 </li>
+                :<h3 key={itemDictKey}>Se quito el filtro</h3>
             )
         })
     })
@@ -31,14 +37,14 @@ const SectionNavAside = ({dictionary, idSection}) => {
     )
 }
 
-const NavbarItemsAside = ({items}) => {
+const NavbarItemsAside = ({items, camaras}) => {
 
     const [dict] = Object.values({items})
     const listSections = dict.map((item, i, array) => {
                 const nameSection = "section_" + Object.keys(item)
                 return (
                     <li key={Object.keys(item)} className="nav-item">
-                        <SectionNavAside dictionary={item} idSection={nameSection}/>
+                        <SectionNavAside dictionary={item} idSection={nameSection} camaras={camaras}/>
                         { i+1 !== array.length && <hr className="d-block my-2 ms-3"/>}
                     </li>
                 )
@@ -63,7 +69,7 @@ const NavbarAside = ({camaras}) => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#f2f2f2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>
             </button>
             <div className="collapse navbar-collapse order-last" id="nav">
-                <NavbarItemsAside items={[filtro_diseno, filtro_resolucion, filtro_tipo_camara]} />
+                <NavbarItemsAside items={[filtro_diseno, filtro_resolucion, filtro_tipo_camara]} camaras={camaras}/>
             </div>
         </nav>
     )
@@ -72,7 +78,7 @@ const NavbarAside = ({camaras}) => {
 function filterAside({camaras}){
     let filtro_diseno = {"diseño":{}}
     let filtro_resolucion = {"resolucion":{}}
-    let filtro_tipo_camara = {"tipo":{}}
+    let filtro_tipo_camara = {"tipo_de_camara":{}}
     
     camaras.map(m => {
 
